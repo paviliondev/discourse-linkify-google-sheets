07afad67b3adf195906767e32e27c8469f917ab5
fzngagan <fzngagan@gmail.com>
2020-08-04 12:45:12 +0530

    FIX: imporved insertion logic and added logic to remove not existant entries

--- app/jobs/scheduled/update_links.rb
+++ app/jobs/scheduled/update_links.rb
@@ -13,9 +13,15 @@ module Jobs
       range = SiteSetting.linkify_google_sheet_name+"!"+SiteSetting.linkify_google_sheet_cell_range
       response = sheets.get_spreadsheet_values spreadsheet_id, range
       return if response.values.empty?
-      response.values.each do |row|
-        PluginStore.set(::LinkifyGoogle::PLUGIN_NAME, row[0], row[1])
+      data = response.values.map do |arr|
+        { plugin_name: ::LinkifyGoogle::PLUGIN_NAME, key: arr[0], value: arr[1], type_name: 'String' }
       end
+
+      PluginStoreRow.insert_all(data);
+
+      keys = data.pluck(:key)
+
+      PluginStoreRow.where(plugin_name: ::LinkifyGoogle::PLUGIN_NAME).where.not(key: keys).destroy_all
     end
   end
 end
